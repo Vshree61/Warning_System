709fa4706473570f29a2cf9dc6bbd35acefb0865
Bipul Shaw <bipulshaw2014@gmail.com>
2020-04-10 19:57:26 +0530

    Added Logout button.

--- app/src/main/java/com/example/warningsystem/MainActivity.java
+++ app/src/main/java/com/example/warningsystem/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.warningsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button logoutBtn;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fAuth = FirebaseAuth.getInstance();
+        logoutBtn = findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),Login.class));
+                finish();
+            }
+        });
     }
 }
