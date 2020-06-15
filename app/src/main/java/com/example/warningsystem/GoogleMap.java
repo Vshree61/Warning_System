75d01a17ddc1886ff984a7e0769b17f81cce8e50
Sayari Bhattacharyya <47933908+Sayari253@users.noreply.github.com>
2020-06-15 16:18:33 +0530

    Update GoogleMap.java

--- app/src/main/java/com/example/warningsystem/GoogleMap.java
+++ app/src/main/java/com/example/warningsystem/GoogleMap.java
@@ -13,21 +13,35 @@ public class GoogleMap{
 	public double lon;
 	DatabaseReference reff;
 	ValueEventListener listener;
-	public void getNearbyLandmark(){
+	public void getNearbyLandmark(double currLatitude,double currLongitude){
+		float distance = Float.MAX_VALUE;
 		reff = FirebaseDatabase.getInstance().getReference();
 		listener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                DataSnapshot landName = dataSnapshot.child("Landmark").child("1");
+				for( DataSnapshot landName : dataSnapshot.child("Landmark").getChildren()){
                 //Landmark Data
                 String name = (String) landName.child("landmark").getValue();
                 Double lonLand = (Double) landName.child("longitude").getValue();
                 Double latLand = (Double) landName.child("latitude").getValue();
                 Long speed = (Long) landName.child("speed").getValue();
-				nameLand = name;
-				speedLand = speed;
-				lat = latLand;
-				lon = lonLand;
+				//distance caluculate
+				Location startPoint = new Location("startPoint");
+                startPoint.setLatitude(currLatitude);
+                startPoint.setLongitude(currLongitude);
+				Location endPoint = new Location("Location B");
+                endPoint.setLatitude(latLand);
+                endPoint.setLongitude(lonLand); 
+				float distance_landmark = startPoint.distanceTo(endPoint);
+				if( distance_landmark < distance && distance_landmark <= 500.0){
+					distance = distance_landmark;
+					nameLand = name;
+					speedLand = speed;
+					lat = latLand;
+					lon = lonLand;
+					}
+				
+				}
 			}
 			@Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
